930c7ceac6a043356a644d166b6dd684e9f94315
andrewfunk14 <therealandyfunk@gmail.com>
2025-12-11 19:05:19 -0700

    Final Fix

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,11 +1,9 @@
-
-
 import { createClient } from "@supabase/supabase-js";
 import Constants from "expo-constants";
 
 const extra = Constants.expoConfig?.extra ?? {};
 
-const supabaseUrl = extra.SUPABASE_URL ?? "";
-const supabaseAnonKey = extra.SUPABASE_ANON_KEY ?? "";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  extra.SUPABASE_URL ?? "",
+  extra.SUPABASE_ANON_KEY ?? ""
+);
