5b3f8a524debb05896c56dc5a49029983fa0206b
andrewfunk14 <therealandyfunk@gmail.com>
2025-12-22 11:59:00 -0700

    fixed supabase variables

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,18 +1,13 @@
 // lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://example.supabase.co';
-const supabaseAnonKey = 'REMOVED_SUPABASE_ANON_KEY';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 
-// import { createClient } from "@supabase/supabase-js";
-// import Constants from "expo-constants";
 
-// const extra = Constants.expoConfig?.extra ?? {};
+import { createClient } from "@supabase/supabase-js";
+import Constants from "expo-constants";
 
-// export const supabase = createClient(
-//   extra.SUPABASE_URL ?? "",
-//   extra.SUPABASE_ANON_KEY ?? ""
-// );
+const extra = Constants.expoConfig?.extra ?? {};
+
+export const supabase = createClient(
+  extra.SUPABASE_URL ?? "",
+  extra.SUPABASE_ANON_KEY ?? ""
+);
