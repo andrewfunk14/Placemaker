65eacbeb03bd8a09c44abbfb088ca0d9819b9cc3
andrewfunk14 <therealandyfunk@gmail.com>
2025-12-22 12:53:35 -0700

    production variables

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,11 +1,25 @@
 // lib/supabaseClient.ts
+
+// // development
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? "";
+// const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "";
+
+// if (!supabaseUrl || !supabaseAnonKey) {
+//   console.warn("Supabase ENV vars are missing. Did you set EXPO_PUBLIC_SUPABASE_URL and EXPO_PUBLIC_SUPABASE_ANON_KEY in .env?");
+// }
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+// production
 import { createClient } from "@supabase/supabase-js";
+import Constants from "expo-constants";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? "";
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "";
+const extra = Constants.expoConfig?.extra ?? {};
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.warn("Supabase ENV vars are missing. Did you set EXPO_PUBLIC_SUPABASE_URL and EXPO_PUBLIC_SUPABASE_ANON_KEY in .env?");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  extra.SUPABASE_URL ?? "",
+  extra.SUPABASE_ANON_KEY ?? ""
+);
